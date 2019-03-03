258f4fbf84ca1e2bd2d0e4c7087b787970ab3b22
whwz <wachowicz@vp.pl>
2019-03-03 14:33:16 +0100

    Updates

---
+++ precache-manifest.d9c607ca9acc542205affa60610c3a23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ef54527c02e253fb74d",
+    "url": "/todo-app/static/js/runtime~main.4ef54527.js"
+  },
+  {
+    "revision": "84297212f774c674b397",
+    "url": "/todo-app/static/js/main.84297212.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/todo-app/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "84297212f774c674b397",
+    "url": "/todo-app/static/css/main.c170cd91.chunk.css"
+  },
+  {
+    "revision": "49c346d636e25f08f0268af4140abfcd",
+    "url": "/todo-app/index.html"
+  }
+];
