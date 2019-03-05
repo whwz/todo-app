a0cbf48c6b4702b29fe47cc4ef762739d2aa5da3
whwz <wachowicz@vp.pl>
2019-03-05 16:33:48 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todo-app/precache-manifest.d9c607ca9acc542205affa60610c3a23.js"
+  "/todo-app/precache-manifest.d68f92b88fef339591a9a5e05df55158.js"
 );
 
 workbox.clientsClaim();
